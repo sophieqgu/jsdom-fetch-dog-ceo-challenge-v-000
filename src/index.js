3b948c89722897a810c61a0e55251c52121ad817
Sophie Gu <sophieqgu@gmail.com>
2021-01-07 04:09:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function fetchDogBreeds() {
       const li = document.createElement('li');
       li.innerText = breed;
       ul.appendChild(li);
-      li.
+      li.addEventListener('click', changeColor)
     }
   })
 }
