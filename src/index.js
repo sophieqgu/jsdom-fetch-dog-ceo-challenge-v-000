36e58d881a6534d01f497e832512e7fc4f90eba4
Sophie Gu <sophieqgu@gmail.com>
2021-01-07 04:02:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetchDogPics();
+  fetchDogBreeds();
+});
+
+
+function fetchDogPics() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(response => response.json())
+  .then(json => {
+    for (const img of json.message) {
+      let element = document.createElement('img');
+      element.src = img;
+      document.body.appendChild(element);
+    }
+  })
+}
+
+function fetchDogBreeds() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(json => {
+    const ul = document.getElementbyId('dog-breeds')
+    for (const breed in json.message) {
+      const li = document.createElement('li');
+      li.innerText = breed;
+      ul.appendChild(li);
+    }
+  })
+}
